ce42d1edf638a1e479e4077e2e83e13dbdf2762d
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-11-05 12:38:36 +0100

    Added __iter__

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from stats import get_num_words
 from stats import dict_to_list
 
 
+class custom_iterable:
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        return iter(self.data)
+
+
 def get_book_text(file_path):
     file_contents = ""
     with open(file_path) as file:
@@ -12,8 +20,10 @@ def get_book_text(file_path):
 def main():
     book = get_book_text("./books/frankenstein.txt")
 
+    iterable_dict = custom_iterable(dict_to_list(book))
+
     list_string = ""
-    for dicts in dict_to_list(book):
+    for dicts in iterable_dict:
         for key, value in dicts:
             if not key.isalpha():
                 continue
